9bad1e24380918049f00c7cd92c76822d311b3a8
SeregaMarkush <s.markushov@icloud.com>
2024-11-11 20:03:41 +0300

    refactor

--- src/main/java/org/example/homeworks/homework1/ArrayRevers.java
+++ src/main/java/org/example/homeworks/homework1/ArrayRevers.java
@@ -10,13 +10,5 @@ public class ArrayRevers {
                 reverseNumbers[j] = numbers[i];
             }
         System.out.println(Arrays.toString(reverseNumbers));
-
-//        for (int i = 0; i < numbers.length / 2; i++) {
-//            int tmp = numbers[i];
-//            numbers[i] = numbers[numbers.length - i - 1];
-//            numbers[numbers.length - i - 1] = tmp;
-//        }
-//        System.out.println(Arrays.toString(numbers));
-
     }
 }
